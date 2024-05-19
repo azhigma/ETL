af2eaa92efb381b8b57127599acc864c605e4ed5
azhigma <nam25nanova@gmail.com>
2024-05-19 22:46:00 +0500

    my commit

--- dags/dag.py
+++ dags/dag.py
@@ -7,6 +7,7 @@ import requests
 import json
 import sqlalchemy
 
+
 dag = DAG(
     dag_id='weather_etl',
     start_date=datetime(2024,4,25),
@@ -14,24 +15,19 @@ dag = DAG(
  )
 
 
-create_table = PostgresOperator(
-        task_id='create_table',
-        sql='sql/create_table.sql',
-        postgres_conn_id='my_postgres'
-    )
-
 # Получение данных о погоде с api
 def get_load_data():
+
     # Необходимые данные для работы с api и базой данных 
     lat = '56.50' #latitude
     lon = '60.35' #longitude
     TOKEN = '' #your unique API key
-
     url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={TOKEN}'
     response = requests.get(url)
     data = response.json()
+    
 
-# Создание списков
+    # Создание списков
     weather = [] 
     temp = []
     wind_speed = []
@@ -41,12 +37,11 @@ def get_load_data():
     today = datetime.now() 
     date.append(today.date()) # извлечение даты и добавление в список
     time.append(today.time()) # извлечение времени и добавление в список
-
     weather.append(data['weather'][0]['main'])
     temp.append(data['main']['temp'])
     wind_speed.append(data['wind']['speed'])
     
- # создание словаря на основе имеющихся списков   
+    # создание словаря на основе имеющихся списков   
     weather_dict = {
         'date': date,
         'time': time,
@@ -55,7 +50,7 @@ def get_load_data():
         'wind_speed': wind_speed
     }
 
-# создание dataframe  на основе словаря
+    # создание dataframe  на основе словаря
     df = pd.DataFrame(weather_dict, columns=['date', 'time', 'description', 'temperature', 'wind_speed'])
     print(df)
 
@@ -72,11 +67,19 @@ def get_load_data():
     engine = sqlalchemy.create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     df.to_sql(name='weather_ekat', con=engine, index=False, if_exists='append')
 
+
+
+create_table = PostgresOperator(
+        task_id='create_table',
+        sql='sql/create_table.sql',
+        postgres_conn_id='my_postgres'
+    )
+
 get_load = PythonOperator(
     task_id='get_data_from_api',
     python_callable=get_data_from_api,
     dag=dag
 )
 
-create_table >> get_load
+create_table >> get_load_data
 
